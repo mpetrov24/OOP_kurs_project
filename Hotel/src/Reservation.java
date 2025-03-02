b61d3f458200dc97c58360b00b0c2e02c94c5613
mpetrov24 <martin.petrov2401@gmail.com>
2025-03-02 17:38:50 +0200

    Implement methods to manage room reservations

--- Hotel/src/Reservation.java
+++ Hotel/src/Reservation.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Reservation {
+    private int roomNumber;
+    private LocalDate from;
+    private LocalDate to;
+    private String note;
+    private int guests;
+
+    public boolean isOverlapping(LocalDate checkFrom, LocalDate checkTo) {
+        return !(checkTo.isBefore(from) || checkFrom.isAfter(to));
+    }
 }
--- Hotel/src/Room.java
+++ Hotel/src/Room.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Room {
+    private int number;
+    private int beds;
+    private boolean isAvailable;
+    private ArrayList<Reservation> reservations;
+
+    public boolean isAvailableOnDate(LocalDate date) {
+        for (Reservation res : reservations) {
+            if (res.isOverlapping(date, date)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addReservation(Reservation res) {
+        reservations.add(res);
+        this.isAvailable = false;
+    }
+
+    public void removeReservation(Reservation res) {
+        reservations.remove(res);
+        this.isAvailable = true;
+    }
 }
