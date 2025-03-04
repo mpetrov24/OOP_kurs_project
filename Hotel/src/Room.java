c6e43921fb40c8f2f31ff4a505572b5404600f5f
mpetrov24 <martin.petrov2401@gmail.com>
2025-03-04 10:34:00 +0200

    Add constructor, getters and setters to Room class

--- Hotel/src/Room.java
+++ Hotel/src/Room.java
@@ -7,6 +7,37 @@ public class Room {
     private boolean isAvailable;
     private ArrayList<Reservation> reservations;
 
+    Room(int number, int beds) throws Exception {
+        setNumber(number);
+        setBeds(beds);
+        this.isAvailable = true;
+        this.reservations = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) throws Exception {
+        if (number > 0) {
+            this.number = number;
+            return;
+        }
+        throw new Exception("Invalid room number");
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) throws Exception {
+        if (beds > 0) {
+            this.beds = beds;
+            return;
+        }
+        throw new Exception("Invalid number of beds");
+    }
+
     public boolean isAvailableOnDate(LocalDate date) {
         for (Reservation res : reservations) {
             if (res.isOverlapping(date, date)) {
